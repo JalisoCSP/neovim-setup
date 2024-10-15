b6b6f26c88cbddbd67aa35e7c13f6596e7c5f2e6
Craig Petterson <cspetterson@hey.com>
2024-10-15 16:36:31 +0100

    Resize dispatch window

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -1,6 +1,12 @@
 return {
   "tpope/vim-rails",
-  "tpope/vim-dispatch",
+  {
+    "tpope/vim-dispatch",
+    config = function()
+      vim.g.dispatch_quickfix_height = 20
+      vim.g.dispatch_tmux_height = 20
+    end
+  },
   {
     "tpope/vim-fugitive",
     config = function()
